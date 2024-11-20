3b5a3268de4bb6551143b2337b9e7e2ba32cd2bb
mmosan <mmosan@fi.uba.ar>
2024-11-20 17:58:55 -0300

    Se puede pasar la ip de cada host por argumento

--- topologia/topology.py
+++ topologia/topology.py
@@ -3,17 +3,23 @@ from mininet.topo import Topo
 
 
 class MyTopo(Topo):
-    def __init__(self, ammount_switches):
-
+    def __init__(
+        self,
+        ammount_switches,
+        ip1="10.0.0.1",
+        ip2="10.0.0.2",
+        ip3="10.0.0.3",
+        ip4="10.0.0.4",
+    ):
         if ammount_switches < 1:
             print("se necesita como minimo un switch")
             return
 
         Topo.__init__(self)
-        h1 = self.addHost("h1")
-        h2 = self.addHost("h2")
-        h3 = self.addHost("h3")
-        h4 = self.addHost("h4")
+        h1 = self.addHost("h1", ip=ip1)
+        h2 = self.addHost("h2", ip=ip2)
+        h3 = self.addHost("h3", ip=ip3)
+        h4 = self.addHost("h4", ip=ip4)
 
         switch = self.addSwitch("s1")
         self.addLink(switch, h1)
